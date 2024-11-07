fcefc0d5a5e391f1847563adfd5b0da8af0566cf
Thomas <tmenouer@gmail.com>
2024-11-07 03:39:37 +0100

    UPD cart

--- src/Controller/Shop/CartController.php
+++ src/Controller/Shop/CartController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller\Shop;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Shop\Products;
+use App\Service\CartService\CartService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/panier', name: 'cart_')]
 class CartController extends AbstractController
 {
-    #[Route('/cart', name: 'cart')]
-    public function index(): Response
+    #[Route('/', name:'index')]
+    public function index(CartService $cartService): Response
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+        return $this->render('cart/cart.html.twig', [
+            'cart' => $cartService->getCart(),
+            'TotalHTC' => $cartService->getPriceHTC()
         ]);
+    }
+
+
+    #[Route('/add/{id}', name: 'add')]
+    public function add(CartService $cartService, #[MapEntity(mapping: ['id' => 'id'])] Products $product): Response{
+
+        $cartService->addProduct($product->getId());
+        
+        return $this->redirectToRoute('cart_index');
+    }
+
+    #[Route('/delete/{id}', name: 'delete')]
+    public function delete(CartService $cartService, #[MapEntity(mapping: ['id' => 'id'])] Products $product): Response{
+
+        $cartService->deleteProduct($product->getId());
+        
+        return $this->redirectToRoute('cart_index');
     }
 }
--- src/Service/CartService/CartService.php
+++ src/Service/CartService/CartService.php
@@ -2,32 +2,119 @@
 
 namespace App\Service\CartService;
 
+use App\Entity\Shop\Products;
+use App\Repository\Shop\ProductsRepository;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 
 class CartService{
 
     private RequestStack $requestStack;
+    private ProductsRepository $productsRepository;
 
-    public function __construct(RequestStack $requestStack) {
+    /**
+     * CartService's constructor
+     * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
+     * @param \App\Repository\Shop\ProductsRepository $productsRepository
+     */
+    public function __construct(RequestStack $requestStack, ProductsRepository $productsRepository) {
 
         $this->requestStack = $requestStack;
+        $this->productsRepository = $productsRepository;
         
     }
 
-    public function addProduct(){
+    /**
+     * Add a product to the cart
+     * @param int $id
+     * @return void
+     */
+    public function addProduct(int $id){
 
-        $this->requestStack->getSession();
-        
+        $cart = $this->requestStack->getSession()->get('cart', []);
+
+        if(empty($cart[$id])){
+            $cart[$id] = 1;
+        }
+        else{
+            $cart[$id]++;
+        }
+
+        $this->getSession()->set('cart', $cart);
 
     }
 
-    public function deleteProduct(){
+    /**
+     * Summary of deleteProduct
+     * @return void
+     */
+    public function deleteProduct(int $id){
+
+        $cart = $this->getSession()->get('panier', []);
+
+        if (!empty($cart[$id])) {
+            unset($cart[$id]);
+        }
+
+        $this->getSession()->set('cart', $cart);
 
 
     }
 
+    /**
+     * Summary of removeCart
+     * @return void
+     */
     public function removeCart(){
 
+    }
+
+    /**
+     * Return the cart
+     * @return array
+     */
+    public function getCart(): array{
+
+        $cart = $this->getSession()->get('cart');
+        $cartData = [];
+
+        foreach($cart as $id => $quantity){
+            $product = $this->productsRepository->find($id);
+
+            $cartData[] = [
+
+                'product' => $product,
+                'quantity' => $quantity
+            ];
+        }
+
+        return $cartData;
+    }
+
+    /**
+     * Return the total price (HTC) of the cart
+     * @return int
+     */
+    public function getPriceHTC(): int{
+
+        $total = 0;
+
+        foreach ($this->getCart() as $item) {
+            
+            $total += $item['product']->getPrice() * $item['quantity'];
+
+        }
+
+        return $total;
+    }
+
+    /**
+     * Return the session
+     * @return \Symfony\Component\HttpFoundation\Session\SessionInterface
+     */
+    private function getSession(): SessionInterface
+    {
+        return $this->requestStack->getSession();
     }
 }
