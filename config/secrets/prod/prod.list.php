375a489a87396b4fc6f0ded55402741a943d097a
ThomasMenouer <tmenouer@gmail.com>
2025-11-19 17:22:10 +0100

    vault

---
+++ config/secrets/prod/prod.list.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'DATABASE_PASSWORD' => null,
+    'INSTAGRAM_APP_ID' => null,
+    'INSTAGRAM_APP_SECRET' => null,
+    'INSTAGRAM_USER_ID' => null,
+    'MAIL_PASSWORD' => null,
+    'STRIPE_SECRET_KEY' => null,
+    'STRIPE_WEBHOOK_SECRET' => null,
+];
